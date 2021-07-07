d1d36ecd10681a8187b2ea85f5c6d9b0c505fe5d
Matt <mattmcguireart@gmail.com>
2021-07-07 16:15:12 -0400

    worked on filter and modal

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
 
-const Header = () => {
-  return <div className="Header">Header works</div>;
+interface Props {
+  setModal: (boolean: boolean) => void;
+}
+
+const Header = ({ setModal }: Props) => {
+  return (
+    <div className="Header">
+      <h1>FlicFavs</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/favorites">Favorites</Link>
+          </li>
+          <li>
+            <Link to="/">Trending</Link>
+          </li>
+          <button onClick={() => setModal(true)}>Discover</button>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
